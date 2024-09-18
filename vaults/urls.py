364be44d2707d8de1fdd10d9015e4c47a99f1d7c
DavidDanso <desiingerboy@icloud.com>
2024-09-18 04:11:04 +0000

    add vault details page with empty state and upload functionality

--- vaults/urls.py
+++ vaults/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('party/911/', views.uploads_view, name='uploads'),
+    path('911/', views.vault_view, name='vault-details'),
 ]
--- vaults/views.py
+++ vaults/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 # 
+def vault_view(request):
+    context = {}
+    return render(request, 'vaults/vault_details.html', context)
+
+
+# 
 def everything_view(request):
     context = {}
     return render(request, 'vaults/everything.html', context)
