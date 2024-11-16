0ae4f2a5fd5237d58b6ae5f1f17be3917cfe7ee9
DavidDanso <desiingerboy@icloud.com>
2024-11-16 15:22:18 +0000

    feat: update Vault timestamp on media upload or deletion

--- vaults/models.py
+++ vaults/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from users.models import Profile
 import uuid
 from django.utils import timezone
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 class Vault(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -14,7 +16,7 @@ class Vault(models.Model):
     def __str__(self):
         return f'{self.title} vault [ {self.owner} ]'
     
-    # display new clients first
+    # display new vaults first
     class Meta:
         ordering = ['-updated_at']
 
@@ -24,8 +26,24 @@ class VaultMedia(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    #
+    def save(self, *args, **kwargs):
+        # Save the media instance
+        super().save(*args, **kwargs)
+        # Update the parent vault's timestamp
+        self.vault.updated_at = timezone.now()
+        self.vault.save()
+
     def __str__(self):
         return f'{self.vault.title} vault media [ {self.vault.owner} ]'
 
     class Meta:
         ordering = ['-updated_at']
+
+
+# Signal to update the Vault's updated_at field when a VaultMedia instance is deleted
+@receiver(post_delete, sender=VaultMedia)
+def update_vault_on_media_delete(sender, instance, **kwargs):
+    vault = instance.vault
+    vault.updated_at = timezone.now()
+    vault.save()
