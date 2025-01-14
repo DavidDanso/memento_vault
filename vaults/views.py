0a1aa08b2c05d55bea2cbb02c22713dfe83737da
DavidDanso <desiingerboy@icloud.com>
2025-01-14 14:07:03 +0000

    feat(vault): limit displayed media tags to the first four for improved layout in gallery

--- vaults/views.py
+++ vaults/views.py
@@ -12,7 +12,6 @@ from django.db.models import Exists, OuterRef
 from django.http import HttpResponse
 from .utils import MediaProcessor
 
-
 GEMINI_API_KEY = settings.GEMINI_API_KEY
 media_processor = MediaProcessor(GEMINI_API_KEY)
 
